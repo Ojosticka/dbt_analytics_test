a79f54dac7b30defc33f9150a2b802f9462def2e
Oluwatobi Ojo <ojotobitemitayo@gmail.com>
2025-11-15 08:55:45 +0000

    Modified customer staging model

--- models/staging/src_customers.sql
+++ models/staging/src_customers.sql
@@ -3,9 +3,9 @@ with warehouse_staging.customers as (
     from warehouse_staging.customers
 )
 
-select  customer_id,
-        customer_unique_id,
+select  CUSTOMER_ID,
+        CUSTOMER_UNIQUE_ID,
         CUSTOMER_ZIP_CODE_PREFIX,
         CUSTOMER_CITY,
-        CUSTOMER_STATE as customer_state_acronym
+        CUSTOMER_STATE AS customer_state_acronym
 from warehouse_staging.customers
